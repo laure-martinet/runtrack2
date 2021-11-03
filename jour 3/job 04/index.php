3836fcafabfb0ec9b0ad1aea131597b0952c8f3a
Martinet Laure <laure.martinet@laplateforme.io>
2021-11-03 14:13:53 +0100

    wsh

---
+++ jour 3/job 04/index.php
@@ -0,0 +1,10 @@
+<?php
+$str = "Dans l'espace, personne ne vous entend crier";
+$x = 0;
+while ($x <= 43)
+{
+    echo "<b>".$str[$x]."</b> est le <b>".$x."</b> caractère <br/>";
+    $x++;
+}
+echo "<br/><b>Il y a ".$x." caractères au total.</b>";
+?>
