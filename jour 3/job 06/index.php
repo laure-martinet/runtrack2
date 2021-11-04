e20ee15e8252ee39e871c55984b7330781382338
Martinet Laure <laure.martinet@laplateforme.io>
2021-11-04 16:41:56 +0100

    nouveau jobsix

--- jour 3/job 06/index.php
+++ jour 3/job 06/index.php
@@ -1,13 +1,4 @@
 <?php
-$str = "Les choses que l'on possede finissent par nous posseder.";
-$x = strlen ($str) -1;
-$output = "";
-
-while ($x >= 0)
-{
-    $output = $output . $str [$x];
-    $x --;
-}
-echo $str; 
-echo "</br>". $output;
+$str = ("Les choses que l'on possede finissent par nous posseder.");
+echo strrev ($str);
 ?>
